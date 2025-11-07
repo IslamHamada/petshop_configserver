e51e04510f4d4b102e70312ce68c29d030a0317a
IslamHamada <Islamhamada222@gmail.com>
2025-11-07 15:39:17 +0100

    add EnableConfigServer annotation

--- src/main/java/com/islamhamada/petshop/ConfigServer.java
+++ src/main/java/com/islamhamada/petshop/ConfigServer.java
@@ -2,8 +2,10 @@ package com.islamhamada.petshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServer {
 
 	public static void main(String[] args) {
